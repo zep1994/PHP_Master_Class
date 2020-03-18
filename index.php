2d10223aace5ef0688ef04f38d5413911a5088b6
Thomas Matlock <32077179+zep1994@users.noreply.github.com>
2020-03-18 15:39:19 -0500

    Add functions

--- index.php
+++ index.php
@@ -10,8 +10,29 @@ and open the template in the editor.
         <title></title>
     </head>
     <body>
+        <main>
+            <header>
+                <h1>Title of the Website</h1>
+            </header>
+            <?php 
+                include 'script.php';
+            ?>
+        </main>
         <?php
+        
             echo 'hello';
+            $arr = ["tim", "<h2>hto</h2>"];
+            
+            foreach ($arr as $id) {
+                echo "</br>";
+                echo $id;
+            }
+            
+            function sayHello() {
+                echo "say hello";
+            }
+            
+            sayHello();
         ?>
     </body>
 </html>
---
+++ script.php
@@ -0,0 +1,16 @@
+<?php
+
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+function greeting() {
+    echo "Hello sucka";
+}
+
+//can use const to make the Constant
+
+greeting();
+
+?>
